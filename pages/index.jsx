9a8285c268be8dfa3e33a023f1e65b69810e675a
SALVADORPOETA <dsml@live.com.mx>
2023-04-21 18:42:09 -0600

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,7 +37,7 @@ export default function Home() {
           <link rel='icon' href='/favicon.ico' />
         </Head>
         {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1] w-full h-full' />
         {/* Background image */}
         <Image 
           src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80' 
@@ -45,7 +45,6 @@ export default function Home() {
           className='object-cover'
           alt='/'
           />
-  
           {/* Search */}
           <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10' >
             <form onSubmit={fetchWeather} className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl' >
@@ -71,3 +70,5 @@ export default function Home() {
   };
 
 }
+
+// Test
