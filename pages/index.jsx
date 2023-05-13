d44bf27125f07ee931b6bdbce21bda7deaac6226
SALVADORPOETA <dsml@live.com.mx>
2023-05-12 19:47:03 -0600

    extras.css2

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import Weather from '../components/Weather';
-import '../styles/extrass.css';
+import '../styles/extras.css';
 // import Spinner from '../components/Spinner';
 
 export default function Home() {
