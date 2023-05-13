713dad24c8081f2fe6c52dd7858381957f660e19
SALVADORPOETA <dsml@live.com.mx>
2023-05-12 18:10:03 -0600

    mb-10%

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -19,7 +19,7 @@ console.log(data)
       </div>
 
       {/* Bottom */}
-      <div className='bg-black/50 relative p-8 rounded-md lg:mb-5 max-lg:mb-5 max-md:mb-9 max-sm:mb-[100px]' >
+      <div className='bg-black/50 relative p-8 rounded-md lg:mb-[10%] max-lg:mb-[10%] max-md:mb-[10%] max-sm:mb-[10%]' >
         <p className='text-2xl text-center pb-6' >Weather in {data.name}</p>
         <div className='flex justify-between text-center'>
           <div>
